2160f17063fb6a4b4c2e7a9cb52de75f4124c028
Arnaud Paran <paran.arnaud@gmail.com>
2021-01-18 17:48:40 +0100

    corrected python logs

--- pyBack/main.py
+++ pyBack/main.py
@@ -1,11 +1,14 @@
 import falcon
 import pyBack.settings
+import logging
 
 class Default:
 
     def on_get(self, req, resp):
+        logging.getLogger(__name__).debug('request', extra={'request': req})
         resp.body = 'Hello World'
         resp.status = falcon.HTTP_200
+
 
 api = falcon.API()
 api.add_route('/hello', Default())
--- pyBack/settings.py
+++ pyBack/settings.py
@@ -10,6 +10,7 @@ try:
     with open('py-logging.yml', 'r') as f:
         LOGGING_SETTINGS = yaml.load(f, yaml.CLoader)
 except FileNotFoundError:
+    logging.getLogger(__name__).exception('Could not find configuration file')
     pass
 
 logging.config.dictConfig(LOGGING_SETTINGS)
